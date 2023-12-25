7c1bcd77c1b3c5169121db71e77f64deff3ee1db
Brunella111 <131973976+Brunella111@users.noreply.github.com>
2023-12-25 15:56:49 +0000

    commit 2

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 
 
+app.get('/', (req, res) => {
+    res.send("backend testing")});
 
-app.listen(3000,(port) => {
-    console.log("App is running on port 3000")
+
+
+mongoose.set("strictQuery",false);
+mongoose.
+connect("mongodb+srv://newadmin:newadmin@nellaapi.rd8z423.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+    console.log('connected to Mongodb')
+    app.listen(3000,(port) => {
+        console.log("App is running on port 3000")
+    });
+}).catch (() => {
+    console.log('error')
 });
