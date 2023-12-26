b1cf1fcd6defb19db59b852baeacd59fd894f259
Brunella111 <131973976+Brunella111@users.noreply.github.com>
2023-12-26 19:32:19 +0000

    Added post endpoints for patient,vitals and encounter creation.

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose');
+//const patient = require('./models/patientModel');
 const app = express();
 
-
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("backend testing")});
+
+app.post('/patient', async(req, res) => {
+    try {
+        const createdPatient = await patient.create(req.body)
+        res.status(200).json(createdPatient);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/encounter', async(req, res) => {
+    try {
+        const createdEncounter = await encounter.create(req.body)
+        res.status(200).json(createdEncounter);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/vitals', async(req, res) => {
+    try {
+        const createdVitals = await vitals.create(req.body)
+        res.status(200).json(createdVitals);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
 
 
 
