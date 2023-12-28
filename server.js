e9ad3afa98769dd9d0188cfc66cd5a72e7f519c4
Brunella111 <131973976+Brunella111@users.noreply.github.com>
2023-12-28 21:06:41 +0000

    feat:Updated system functionality to include GET request to view details of a specific patient.

--- server.js
+++ server.js
@@ -16,10 +16,24 @@ app.get('/patient', async(req, res) => {
 })
 
 
+
 app.post('/patient', async(req, res) => {
     try {
         const createdPatient = await patient.find(req.body)
         res.status(200).json(createdPatient);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//route to view details of a specific patient
+
+app.post('/patient/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const patient = await patient.findById(id);
+        res.status(200).json(patient);
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message})
