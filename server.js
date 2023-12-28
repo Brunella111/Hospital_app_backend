5b39b84a8d2a924e9f8b1b84e9ac9192e8f4339f
Brunella111 <131973976+Brunella111@users.noreply.github.com>
2023-12-28 21:00:15 +0000

    feat:Updated system functionality to include GET requests to view list of patients.

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("backend testing")});
+app.get('/patient', async(req, res) => {
+    try {
+        const viewPatients = await patient.create(req.body)
+        res.status(200).json(viewPatients);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
 
 app.post('/patient', async(req, res) => {
     try {
-        const createdPatient = await patient.create(req.body)
+        const createdPatient = await patient.find(req.body)
         res.status(200).json(createdPatient);
     } catch (error) {
         console.log(error.message);
